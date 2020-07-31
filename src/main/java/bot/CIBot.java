9cbcc7e9a950120a0623bacb84e8c754814efd07
MathematicMan <vladviv91@gmail.com>
2020-08-01 00:16:53 +0300

    added logic

--- src/main/java/bot/CIBot.java
+++ src/main/java/bot/CIBot.java
@@ -1,4 +1,34 @@
 package bot;
 
-public class CIBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class CIBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText() ) {
+            String messageText = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage().setChatId(chat_id).setText(messageText);
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Helper";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "123654789:ASD_QWERYTMZXCASG";
+    }
 }
--- src/main/java/bot/CIBotMain.java
+++ src/main/java/bot/CIBotMain.java
@@ -1,4 +1,18 @@
 package bot;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class CIBotMain {
+    public static void  main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botApi = new TelegramBotsApi();
+
+        try {
+            botApi.registerBot(new CIBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
 }
