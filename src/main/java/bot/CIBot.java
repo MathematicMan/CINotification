c77bf0ef25fc962659c22c1a33d92b6cbdb82842
Vlad Isaev <vladviv91@gmail.com>
2020-08-20 17:18:46 +0300

    added work with photo

--- src/main/java/bot/CIBot.java
+++ src/main/java/bot/CIBot.java
@@ -2,25 +2,50 @@ package bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import java.io.File;
 
 public class CIBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText() ) {
-            String messageText = update.getMessage().getText();
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String incomingMessage = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
-
-            SendMessage message = new SendMessage().setChatId(chat_id).setText(messageText);
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+            if (incomingMessage.startsWith("[Hey boy]")) {
+                String messageText = update.getMessage().getText();
+                SendMessage message = new SendMessage().setChatId(chat_id).setText(messageText);
+                sendTextMessage(message);
+            }
+            if (incomingMessage.equals("hello there")) {
+                File photoFile = new File("../resources/greevos.jpg");
+                SendPhoto photo = new SendPhoto().setChatId(chat_id).setPhoto(photoFile);
+                System.out.println("Absolute file path: " + photoFile.getAbsolutePath());
+                System.out.println("Pathname stringL:" +  photoFile.getPath());
+                System.out.println("URI:" +  photoFile.toURI());
+                sendPhoto(photo);
             }
         }
     }
+
+    private void sendTextMessage(final SendMessage message){
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendPhoto(final SendPhoto photo) {
+        try {
+            execute(photo);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public String getBotUsername() {
